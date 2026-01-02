c4574ecd68f0bb451c0e339045345fba7f3731e2
matias <ma.antoniorodriguez@gmail.com>
2026-01-02 01:51:18 -0300

    avances

--- app/calavera-de-vaca/page.tsx
+++ app/calavera-de-vaca/page.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Titulo from "@/components/Titulo";
+import "./style.css";
 
 export default function Home() {
   return (
         <div className="page">
             <Header/>
-            
-            <img src="calavera.png"></img>
+            <h1>Calavera de Vaca</h1>
+            <div className="contenido">
+                <div className="texto">
+                    Es muy usual encontrar colgadas en las paredes o en algún tronco una calavera de vaca o toro en las casas rurales. Existe la creencia de que este elemento funciona como un 'contra' para las malas intenciones o hechizo de quienes puedan llegar a la casa, tal como la cruz de parqui amarrada con lana roja o tener una planta de  ruda a la entrada de la casa
+                </div>
+                <div className="imagen">
+                    <img src="calavera.png"></img> 
+                </div>
+            </div>
 
             <Footer/>
         </div>
