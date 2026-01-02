b8a4bf95001575209c3533cacc337c68b025e1a3
matias <ma.antoniorodriguez@gmail.com>
2026-01-02 00:41:01 -0300

    Inicial

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Main from "@/components/Main";
+
+export default function Home() {
+  return (
+    <div className="page">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
---
+++ components/Main.tsx
@@ -0,0 +1,19 @@
+import "./../styles/Main.css"
+import Image from "next/image";
+
+export default function Main() {
+    return (
+        <main>
+            <div className="titulo-div">
+                <div className="titulo-texto">
+                    <h1>
+                        Pamela Valentina
+                    </h1>
+                    Soy una fotografa
+                </div>
+                <img src="/vale.png"></img>
+            </div>
+            Main
+        </main>
+    );
+}
